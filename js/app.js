2d6192138b85c1c0761a71fc4f6363b0c4c05850
Eduardo Garza <e.garza2505@gmail.com>
2023-10-24 15:17:00 -0600

    menu scroll

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
-const menu = document.querySelector('.nav__menu');
-const menuList = document.querySelector('.nav__list');
-const links = document.querySelectorAll('.nav__link');
+const menu = document.querySelector(".nav__menu");
+const menuList = document.querySelector(".nav__list");
+const links = document.querySelectorAll(".nav__link");
 
-menu.addEventListener('click', function(){
-    menuList.classList.toggle('nav__list--show');
+menu.addEventListener("click", function () {
+	menuList.classList.toggle("nav__list--show");
+	document.body.classList.toggle("body-no-scroll");
 });
 
-links.forEach(function(link) {
-    link.addEventListener('click', function() {
-        menuList.classList.remove('nav__list--show');
-        document.body.classList.remove('no-scroll'); // Elimina la clase no-scroll del body al hacer clic en un enlace
-    });
+links.forEach(function (link) {
+	link.addEventListener("click", function () {
+		menuList.classList.remove("nav__list--show");
+		document.body.classList.remove("body-no-scroll");
+	});
 });
